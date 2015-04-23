b1c202d138ec482c358ff315305ecdd8d8d378ff
gamaliellobato <gamaliellobato@gmail.com>
2015-04-23 12:38:08 -0500

    06. Se agrega funcionalidad de compartir link y se carga bootstrap con exito

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -18,15 +18,23 @@ modulo1.controller("mainCtrl",[
 		$scope.addPost = function (){
 			if(!$scope.title || $scope.title === "")
 			{
-				alert("No se pertite postear titulos vacios");
+				alert("No se permite postear titulos vacios");
 				return;
 			}
 			$scope.posts.push(
 				{
 					title: $scope.title,
+					link: $scope.link,
 					upvotes:0
 				});
 			//Two-way data binding
 			$scope.title = "";
+			$scope.link = "";
+		};
+		//Metodo que incrementa el voto
+		//de un post en una unidad
+		$scope.incrementUpvotes = function (post)
+		{
+			post.upvotes += 1;
 		};
 	}]);
