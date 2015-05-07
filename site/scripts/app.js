2a703e3d018f641df5eb033ee15d3ceff9865354
gamaliellobato <gamaliellobato@gmail.com>
2015-05-07 13:00:18 -0500

    09. Se agrega un servicio de tipo factory para almacenar los posts

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,18 +1,42 @@
 console.log ("#Gama: Cargo app.js");
 var modulo1=
 	angular.module("reeditgam",[]);
+	//creando un servicio del tipo factory
+modulo1.factory('posts',[function(){
+	//cuerpo delf actory llamdo post
+	var o = {
+		posts : [
+			{
+				title: "post 1", upvotes: 15,
+				comments: [
+					{author: "Karina", body: "Esto esta de pelos.",
+					upvotes:3},
+					{author: "Gamaliel", body: "Esto es basura.",
+					upvotes:0}]
+			},
+			{
+				title: "post 2", upvotes: 15,
+				comments: [
+					{author: "Coco", body: "Esto es asombroso.",
+					upvotes:5},
+					{author: "Cristian", body: "Esto esta aburrido.",
+					upvotes:1}]
+			}
+		]
+	};
+	//retornando objeto de datos persistentes
+	return o;
+}]);
+
+	//creando controlador
+	//dependecy injections
 modulo1.controller("mainCtrl",[
-	'$scope',
-	function($scope){
+	'$scope','posts',//inyectando factory post
+	function($scope, posts){
 		$scope.test = "Hola Angular";
-		//Modelo
-		$scope.posts = 
-		[{title: "Post 1", upvotes: 5},
-		{title: "Post 2", upvotes: 15},
-		{title: "Post 3", upvotes: 7},
-		{title: "Post 4", upvotes: 9},
-		{title: "Post 5", upvotes: 3},
-		{title: "Post 6", upvotes: 5}];
+		//Modelo al cual se le asigna
+		//el resultado del factory
+		$scope.posts = posts.posts;
 
 		//Metodo del controlador
 		$scope.addPost = function (){
